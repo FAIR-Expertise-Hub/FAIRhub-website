2e2114e58d6cb9fd6b9b8403326ed2fecfa6486c
Shuai Wang <shuaiwang@Shuais-MacBook-Air.local>
2023-09-11 13:58:51 +0200

    updating settings file

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -18,6 +18,8 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Before using your Heroku app in production, make sure to review Django's deployment checklist:
@@ -169,8 +171,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = "static/"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
 
 STORAGES = {
     # Enable WhiteNoise's GZip and Brotli compression of static assets:
